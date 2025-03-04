9b4fa54526b40a52145bd5d6421f0c94e2c29f07
Broshe4ka <gorelovsergey13@gmail.com>
2025-03-04 21:38:54 +0600

    Add Map section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Welcome from '@/components/sections/welcome/welcome';
 import About from '@/components/sections/about/about';
 import Subs from '@/components/sections/subs/subs';
 import Faq from '@/components/sections/faq/faq';
+import Map from '@/components/sections/map/map';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Welcome />
       <About />
       <Subs />
+      <Map />
       <Faq />
     </>
   );
---
+++ src/components/sections/map/map.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { useState } from 'react';
+import styles from './map.module.scss';
+
+export default function Map() {
+  const [isActive, setIsActive] = useState(false);
+  return (
+    <section className={`${styles.map} section container`}>
+      <h2 className="section__title">
+        Оцените постройки наших игроков прямо из браузера!
+      </h2>
+      <div
+        className={styles.wrapper}
+        onMouseLeave={() => setIsActive(false)}
+      >
+        <div
+          className={`${styles.overlay} ${
+            isActive ? styles.hidden : ''
+          }`}
+          onClick={() => setIsActive(true)}
+        >
+          <span>Кликните чтобы начать взаимодействие</span>
+        </div>
+        <iframe
+          className={`${styles.mapIframe} ${
+            isActive ? styles.isActive : ''
+          }`}
+          src="http://localhost:8100"
+          allowFullScreen
+        />
+      </div>
+    </section>
+  );
+}
